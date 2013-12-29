8914f534b4c51819255ece6b65fd7acba6f3a95a
Jason Conklin <j@ninly.net>
2013-12-29 13:12:38 -0600

    Add .Xresources to tracked dotfiles; mod symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile bash_prompt bash_aliases vimrc" # list of files/folders to symlink in homedir
+files="bashrc bash_profile bash_prompt bash_aliases vimrc Xresources" # list of files/folders to symlink in homedir
 
 ##########
 
